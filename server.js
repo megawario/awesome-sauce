6cd42a983f640933ca9ccd99606773199a783eb6
Pedro Oliveira <pedro.oliv16@gmail.com>
2016-02-07 15:41:51 +0000

    Cleanup debug code and bugfixes

--- server.js
+++ server.js
@@ -38,73 +38,59 @@ var sessionSchema = new Schema({
     cookieID: String
 });
 
-/*sessionSchema.statics.findOrCreate = function(id, cb){
-    var query = 
-    return this.find({_id: id}, cb);
-    
-};*/
-
 var User = mongoose.model('sessions', sessionSchema);
 
+//Google OAuth
 passport.use(new googleStrategy({
-   clientID:  config.auth.google.clientID,
+    clientID:  config.auth.google.clientID,
     clientSecret: config.auth.google.clientSecret, 
     callbackURL: config.auth.google.callbackURL,
     scope: config.auth.google.scope,
     passReqToCallback: true
 
-},
-
-				function(req, accessToken, refreshToken, profile, done){
-				    
-				    //console.log("AUTH: identifier is" + identifier);
-				    
-				    console.log("AUTH: got access token "+accessToken);
-				    console.log("AUTH: got refresh token "+refreshToken);
-				    console.log("AUTH: profile info given: "+profile.id);
-				    User.findOne({'userID': profile.id}, function(err, user){
-					if(!user){
-					    console.log("User NOT FOUND on session store :(\nCreating User!");
-					    
-					    user = new User({
-						displayName: profile.displayName,
-						email: profile.email,
-						userID: profile.id,
-						provider: 'google',
-						accessToken: accessToken,
-						cookieID: ''
-					    });
-					    user.save(function(err){
-						if(err) console.log(err);
-						return done(err, user);
-						
-					    });
-					}
-					
-					else{
-					    console.log("User FOUND on session store! userID ", profile.id);
-					    return done(null, user);
-					}
-					
-					
-				    });
-				}));
-
-					
+}, function(req, accessToken, refreshToken, profile, done){
+    
+    //console.log("AUTH: identifier is" + identifier);
+    
+    console.log("AUTH: got access token "+accessToken);
+    console.log("AUTH: got refresh token "+refreshToken);
+    console.log("AUTH: profile info given: "+profile.id);
+    User.findOne({'userID': profile.id}, function(err, user){
+	if(!user){
+	    console.log("User NOT FOUND on session store :(\nCreating User!");
 	    
-	    
+	    user = new User({
+		displayName: profile.displayName,
+		email: profile.email,
+		userID: profile.id,
+		provider: 'google',
+		accessToken: accessToken,
+		cookieID: ''
+	    });
+	    user.save(function(err){
+		if(err) console.log(err);
+		return done(err, user);
+		
+	    });
+	}
+	
+	else{
+	    console.log("User FOUND on session store! userID ", profile.id);
+	    return done(null, user);
+	}
+	
+	
+    });
+}));
 
-passport.serializeUser(function(user, cb) {
-    //console.log("Serialize: ",user);
+
+passport.serializeUser(function(user, cb) { //inserts user id into the session store @.sessions.passport.user
     cb(null, user.userID);
     
 });
 
 passport.deserializeUser(function(obj, cb) {
-    //    console.log("Deserialize ",obj);
-    //User.findById(id, function(err, user){	
-	cb(null, obj);
-    //});
+    cb(null, obj);
 });
 
 app.use(cookieParser());
@@ -114,7 +100,10 @@ app.use(session({
     },
     secret: 'keyboard cat',
     name: 'session.sid',
-
+    cookie:{
+	httpOnly: false,
+	maxAge: 30 * 24 * 60 * 60 * 1000 // 1 month
+    },
     resave: false,
     saveUninitialized: false,
     store: new mongoStore({url: config.db.url, collection: 'cookies'})
@@ -128,10 +117,10 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req,res){ //delete when a properly configured webserver is put in place
-    
-    
-    if(!req.session){
+        
+    if(!req.session.passport){
 	console.log("Cookie not set, user not logged in");
+	console.log(req.session);
     }
     else{
 	console.log("Cookie set, user logged in. User id:");
@@ -153,18 +142,10 @@ app.get('/', function(req,res){ //delete when a properly configured webserver is
 
 app.get('/auth/google', passport.authenticate('google'));
 app.get('/auth/google/return',
-	passport.authenticate('google', { successRedirect: '/auth/google/postauth',
+	passport.authenticate('google', { successRedirect: '/',
 					  failureRedirect: '/error' }));
 
-app.get('/auth/google/postauth', function(req, res){ //do some auth post processing that can only happen after the cookie is set.
-    var cookieID = req.cookies['session.sid'].slice(2, req.cookies['session.sid'].length);//get rid of the 's:' prefix that express-session prepends to the cookie id
-    cookieID = cookieID.slice(0,cookieID.indexOf('.'));
-    console.log(cookieID);
-    console.log(res.session);
 
-    res.redirect("/");
-});
-//	console.log(req.session);
 //Services
 //POST
 app.use(bodyParser.json());
@@ -173,8 +154,8 @@ app.use(bodyParser.json());
 app.post('/rest/adventure/create',function(req,res){
     console.log('post request from '+req.ip+' to ' +req.path);
     console.log("isAuthenticated? "+req.isAuthenticated());
-    console.log("USER:"+req.session.id);
-    console.log("SESSION DATA: "+req.session.cookie.id);
+    console.log("USER:"+req.session.passport.user);
+
     //TODO process validation for the req body here.
     db.createAdventure(req.body,function(err,docId){
 	if(err){
