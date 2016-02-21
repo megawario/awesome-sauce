655b87265f9259bf283fff24694ffe143c7f9601
megawario <mpinto011@gmail.com>
2016-02-21 04:59:08 +0100

    added new fields

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config={};
 
 //server configurations
 config.server={};
-config.server.port=9191;
+config.server.port=8080;
 config.server.path='/lisrpg';
 config.server.serveStatic=true; //serve static pages using nodejs use false when using nginx
 config.server.debug=true; //set debug env var for nodeJS. use false for prod. This sets inside the working process.
@@ -15,9 +15,9 @@ config.db.url='mongodb://localhost:27017/merge';
 //auth config
 config.auth={};
 config.auth.google={};
-config.auth.google.clientID='819734387202-0uu7puep4peo0kibf7taattcqjcp6d09.apps.googleusercontent.com'; //api key
-config.auth.google.clientSecret='ws5fhdEprHtfPHU-P328xYAx'; //api password
-config.auth.google.callbackURL='http://thebard.pt:9191/lisrpg/auth/google/return'; 
+config.auth.google.clientID=''; //api key
+config.auth.google.clientSecret=''; //api password
+config.auth.google.callbackURL='http://thebard.pt:8080/lisrpg/auth/google/return'; 
 config.auth.google.scope='https://www.googleapis.com/auth/userinfo.email';
 
 
